8b682d8f3076cbef516d1e103101c03903bfb46c
richardlm57 <richardlm57@gmail.com>
2015-09-29 20:03:25 -0430

    Environment

---
+++ true.hs
@@ -0,0 +1,22 @@
+type Environment = [(String,Bool)]
+
+find :: Environment -> String -> Maybe Bool
+find [] _ = Nothing
+find ((x,y):l) k
+	| x == k 	= Just y
+	| otherwise 	= find l k
+
+addOrReplace :: Environment -> String -> Bool -> Environment
+addOrReplace l x b = addAux l [] x b
+	where 	addAux [] l x b = (x,b):reverse l
+		addAux (t@(l,m):n) q x b 
+			| l == x 	= reverse ((x,b):q) ++ n
+			| otherwise	= addAux n (t:q) x b
+
+remove :: Environment -> String -> Environment
+remove e k = remAux e k []
+	where 	remAux [] _ l = reverse l
+		remAux (t@(x,y):m) k l 
+			| x==k 		= remAux m k l
+			| otherwise	= remAux m k ((x,y):l)
+a = [("x",True),("y",False)] :: Environment
