ac4964819de420e3f4459e804d57166c275d789a
“Patricia“ <patricia.reinoso12@gmail.com>
2015-10-01 17:52:16 -0430

    Show de Suit, Value, Card

--- Cards.hs
+++ Cards.hs
@@ -1,15 +1,35 @@
-data Suit = Clubs | Diamonds | Spades | Hearts deriving (Show)
+data Suit = Clubs | Diamonds | Spades | Hearts deriving(Read)
 
-data Value = Numeric Int | Jack | Queen | King | Ace deriving (Show)
+instance Show Suit where
+	show Clubs    = "C" 
+	show Diamonds = "D" 
+	show Spades   = "S" 
+	show Hearts   = "H"
+
+data Value = Numeric Int | Jack | Queen | King | Ace deriving (Read)
+
+instance Show Value where
+	-- show (Numeric Int) = (Numeric Int)
+	show Jack 	= "J"
+	show Queen 	= "Q"
+	show King 	= "K"
+	show Ace 	= "A"
 
 data Card = Card {
 					value :: Value,
 					suit :: Suit
 				 }
-				 deriving (Show)
+				 deriving (Read)
+
+instance Show Card where
+	--show (Card v s) = show s ++ show v
+	show c = (show.suit)c ++ (show.value)c
 
 newtype Hand = H [Card]
-				deriving (Show)
+				deriving (Show, Read)
+
+--instance Show Hand where
+--	show (H (x:xs)) = show xs ++ show x 
 
 empty :: Hand
 
@@ -18,3 +38,10 @@ empty = H []
 size :: Hand -> Int
 
 size (H h) = length h
+
+x = Diamonds
+y = Jack
+
+z = Card y x
+
+w = H [z,z,z]
